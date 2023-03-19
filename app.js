83f8f82df140bcd54531e59c882525fa56c64d1d
Yadola <Ya-dola@users.noreply.github.com>
2023-03-19 21:52:13 +0530

    Added Colouring Modes

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const MODE = {
 
 const COLOUR = {
     DEFAULT: '#1f1f1f',
-    ERASER: '#eaeaea',
-    RAINBOW: `rgb(${randColourChannel()}, ${randColourChannel()}, ${randColourChannel()})`
+    ERASER: '#eaeaea'
 };
 
 // Constants
@@ -26,8 +25,8 @@ const txtSize = document.getElementById('txtSize');
 const sliderSize = document.getElementById('sliderSize');
 
 // Variables
-let colour = DEFAULT_COLOUR;
-let mode = DEFAULT_MODE;
+let selectedColour = DEFAULT_COLOUR;
+let selectedMode = DEFAULT_MODE;
 let gridSize = DEFAULT_GRID_SIZE;
 let mousePressed = false;
 
@@ -37,6 +36,7 @@ defaultSetup();
 // Functions
 function defaultSetup() {
     populateGrid(DEFAULT_GRID_SIZE);
+    setMode(DEFAULT_MODE);
 }
 
 function randColourChannel() {
@@ -64,9 +64,73 @@ function createGridElement() {
 function colourElement(evt) {
     // If only hovering but not Pressing = DO NOTHING
     if (evt.type === 'mouseover' && !mousePressed) return;
-    evt.target.style.backgroundColor = colour;
+    evt.target.style.backgroundColor = selectedColour;
+
+    if (selectedMode === MODE.RAINBOW)
+        evt.target.style.backgroundColor
+            = `rgb(${randColourChannel()}, ${randColourChannel()}, ${randColourChannel()})`;
+}
+
+function setMode(mode) {
+    deactivateActiveButton();
+    setActiveButton(mode);
+    setColour(mode);
+
+    selectedMode = mode;
+}
+
+function deactivateActiveButton() {
+    switch (selectedMode) {
+        case MODE.RAINBOW:
+            btnRainbow.classList.toggle('active');
+            break;
+        case MODE.ERASER:
+            btnEraser.classList.toggle('active');
+            break;
+        default:
+            btnDefault.classList.toggle('active');
+            break;
+    }
+}
+
+function setActiveButton(mode) {
+    switch (mode) {
+        case MODE.RAINBOW:
+            btnRainbow.classList.toggle('active');
+            break;
+        case MODE.ERASER:
+            btnEraser.classList.toggle('active');
+            break;
+        default:
+            btnDefault.classList.toggle('active');
+            break;
+    }
+}
+
+function setColour(mode) {
+    switch (mode) {
+        case MODE.RAINBOW:
+            selectedColour = '';
+            break;
+        case MODE.ERASER:
+            selectedColour = COLOUR.ERASER;
+            break;
+        default:
+            selectedColour = COLOUR.DEFAULT;
+            break;
+    }
 }
 
 // Event Listeners
 document.body.onmousedown = () => (mousePressed = true);
 document.body.onmouseup = () => (mousePressed = false);
+
+btnDefault.addEventListener('click', () => {
+    setMode(MODE.DEFAULT);
+});
+btnRainbow.addEventListener('click', () => {
+    setMode(MODE.RAINBOW);
+});
+btnEraser.addEventListener('click', () => {
+    setMode(MODE.ERASER);
+});
