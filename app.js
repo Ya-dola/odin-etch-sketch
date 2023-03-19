eb0ee0d0af632f3e08d1fbfd727821ad01ac9bb0
Yadola <Ya-dola@users.noreply.github.com>
2023-03-19 21:05:28 +0530

    Added Logic to Populate Grid

--- app.js
+++ app.js
@@ -1,7 +1,64 @@
+// ENUMS
+const MODE = {
+    DEFAULT: 'default',
+    RAINBOW: 'rainbow',
+    ERASER: 'eraser'
+};
+
+const COLOUR = {
+    DEFAULT: '#1f1f1f',
+    ERASER: '#eaeaea',
+    RAINBOW: `rgb(${randColourChannel()}, ${randColourChannel()}, ${randColourChannel()})`
+};
+
+// Constants
+const DEFAULT_COLOUR = COLOUR.DEFAULT;
+const DEFAULT_MODE = MODE.DEFAULT;
+const DEFAULT_GRID_SIZE = 16;
+
+// Page Elements
+const grid = document.getElementById('grid');
+const btnDefault = document.getElementById('btnDefault');
+const btnRainbow = document.getElementById('btnRainbow');
+const btnEraser = document.getElementById('btnEraser');
+const btnReset = document.getElementById('btnReset');
+const txtSize = document.getElementById('txtSize');
+const sliderSize = document.getElementById('sliderSize');
+
 // Variables
+let colour = DEFAULT_COLOUR;
+let mode = DEFAULT_MODE;
+let gridSize = DEFAULT_GRID_SIZE;
 
 // Main Execution
+defaultSetup();
 
 // Functions
+function defaultSetup() {
+    populateGrid(DEFAULT_GRID_SIZE);
+}
+
+function randColourChannel() {
+    return Math.floor((Math.random() * 256));
+}
+
+function populateGrid(gridSize) {
+    // Both Rows and Columns size set at once
+    grid.style.gridTemplate = `repeat(${gridSize}, 1fr) / repeat(${gridSize}, 1fr)`;
+
+    for (let i = 0; i < gridSize * gridSize; i++)
+        grid.appendChild(createGridElement());
+}
+
+function createGridElement() {
+    const gridElement = document.createElement('div');
+    gridElement.classList.toggle('gridElement');
+    gridElement.addEventListener('mousedown', colourElement);
+    return gridElement;
+}
+
+function colourElement() {
+
+}
 
 // Event Listeners
